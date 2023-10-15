41935f15aea6aea6d466fee13b98bf22441a002c
TeHD <dangkhoa014@gmail.com>
2023-10-15 18:18:51 +0700

    init commit

---
+++ mediasocial/src/test/java/com/groupnine/mediasocial/MediasocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupnine.mediasocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediasocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
