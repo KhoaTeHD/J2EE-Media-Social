6410400d795eae6727a7f66d0e4a1604a9c993b7
TeHD <dangkhoa014@gmail.com>
2023-10-24 00:15:47 +0700

    update cấu trúc file

---
+++ mediasocial/src/main/java/com/groupnine/mediasocial/entity/FriendRequest.java
@@ -0,0 +1,5 @@
+package com.groupnine.mediasocial.entity;
+
+public class FriendRequest {
+
+}
---
+++ mediasocial/src/main/java/com/groupnine/mediasocial/repository/FriendRequestRepository.java
@@ -0,0 +1,5 @@
+package com.groupnine.mediasocial.repository;
+
+public class FriendRequestRepository {
+
+}
