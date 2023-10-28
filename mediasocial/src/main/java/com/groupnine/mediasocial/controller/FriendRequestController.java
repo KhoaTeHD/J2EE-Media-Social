898e228fe49178db6c5f2feaaaab57fd8b31d08a
TeHD <dangkhoa014@gmail.com>
2023-10-28 23:57:14 +0700

    rename

--- mediasocial/src/main/java/com/groupnine/mediasocial/controller/FriendRequestController.java
+++ mediasocial/src/main/java/com/groupnine/mediasocial/controller/FriendRequestController.java
@@ -1,6 +1,7 @@
 package com.groupnine.mediasocial.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,5 +16,10 @@ public class FriendRequestController {
 	@ResponseBody
 	public FriendRequestDTO createFR(@RequestBody FriendRequestDTO model) {		
 		return model;
+	}
+	
+	@GetMapping("/getFriendsById")
+	public String getFriendsByID() {
+		return null;
 	}
 }
