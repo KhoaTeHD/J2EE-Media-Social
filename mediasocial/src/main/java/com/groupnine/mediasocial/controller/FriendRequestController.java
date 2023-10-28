714ee6f1791a5ecf6f0f086498ae3904be1491e4
KhoaTeHD <dangkhoa014@gmail.com>
2023-10-28 22:37:33 +0700

    Khoa up recommed page

--- mediasocial/src/main/java/com/groupnine/mediasocial/controller/FriendRequestController.java
+++ mediasocial/src/main/java/com/groupnine/mediasocial/controller/FriendRequestController.java
@@ -1,9 +1,8 @@
 package com.groupnine.mediasocial.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.groupnine.mediasocial.dto.FriendRequestDTO;
@@ -11,7 +10,7 @@ import com.groupnine.mediasocial.dto.FriendRequestDTO;
 @Controller
 public class FriendRequestController {
 	
-	@RequestMapping(value ="/friendrequest", method = RequestMethod.POST)
+	@PostMapping("/friendrequest")
 	@ResponseBody
 	public FriendRequestDTO createFR(@RequestBody FriendRequestDTO model) {		
 		return model;
