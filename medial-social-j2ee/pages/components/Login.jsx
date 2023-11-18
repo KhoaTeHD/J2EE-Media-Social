1abeed991d949cddad2542ae9fc250420cce3e6c
NhuLe1101 <104067198+NhuLe1101@users.noreply.github.com>
2023-11-18 22:46:31 +0700

    Nhu Update Login.jsx

--- medial-social-j2ee/pages/components/Login.jsx
+++ medial-social-j2ee/pages/components/Login.jsx
@@ -1,7 +1,69 @@
 import styles from '@/styles/Login.module.css'
 import Link from 'next/link'
+import { useState } from 'react';
+import AuthService from './../api/auth-service';
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { useRouter } from 'next/router'
+import { useRef } from 'react';
 
+const required = (value) => {
+    if (!value) {
+        return (
+            <div className={styles.error} role="alert">
+                Không được để trống!
+            </div>
+        );
+    }
+};
 const Login = () => {
+    const router = useRouter();
+    const form = useRef();
+    const checkBtn = useRef();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+    const onChangeEmail = (e) => {
+        const email = e.target.value;
+        setEmail(email);
+    };
+
+    const onChangePassword = (e) => {
+        const password = e.target.value;
+        setPassword(password);
+    };
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        setMessage("");
+        setLoading(true);
+
+        form.current.validateAll();
+
+        if (checkBtn.current.context._errors.length === 0) {
+            AuthService.login(email, password).then(
+                () => {
+                    router.push('Homepage');
+                },
+                (error) => {
+                    const resMessage =
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString();
+
+                    setLoading(false);
+                    setMessage(resMessage);
+                }
+            );
+        } else {
+            setLoading(false);
+        }
+    };
     return (
         <div className={styles.container}>
             <div className={styles.login}>
@@ -16,18 +78,42 @@ const Login = () => {
                     </div>
                     <div className={styles.right}>
                         <div className={styles.title}>Đăng nhập</div>
-                        <div className={styles.input_box}>
-                            <input className={styles.input} type="text" required/>
-                            <label className={styles.text_input} htmlFor="">Tài khoản</label>
-                            <div className={styles.underline} ></div>
-                        </div>
-                        <div className={styles.input_box}>
-                            <input className={styles.input} type="text" required/>
-                            <label className={styles.text_input} htmlFor="">Mật khẩu</label>
-                            <div className={styles.underline} ></div>
-                        </div>
-                        <button className={styles.btn}>Đăng nhập</button>
-
+                        <Form onSubmit={handleLogin} ref={form} className={styles.form}>
+                            <div className={styles.input_box}>
+                                <Input className={styles.form_input} type="text"
+                                    name="email"
+                                    value={email}
+                                    onChange={onChangeEmail}
+                                    validations={[required]}
+                                />
+                                <label className={styles.text_input} htmlFor="">Tài khoản</label>
+                                {/* <div className={styles.underline} ></div> */}
+                            </div>
+                            <div className={styles.input_box}>
+                                <Input className={styles.form_input} type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={onChangePassword}
+                                    validations={[required]}
+                                />
+                                <label className={styles.text_input} htmlFor="">Mật khẩu</label>
+                                {/* <div className={styles.underline} ></div> */}
+                            </div>
+                            <button className={styles.btn} disabled={loading}>
+                                {loading && (
+                                      <i class="fa fa-circle-o-notch fa-spin"></i>
+                                )}
+                                <span>Login</span>
+                            </button>
+                            {message && (
+                                <div className={styles.message}>
+                                    <div className={styles.alert} role="alert">
+                                        {message}
+                                    </div>
+                                </div>
+                            )}
+                            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+                        </Form>
                     </div>
                 </div>
             </div>
